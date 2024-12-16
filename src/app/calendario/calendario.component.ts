51c02efd15b602716258e38da619bf3e0759e905
XitlaliReyes <al262973@edu.uaa.mx>
2024-12-15 20:12:59 -0600

    implementacion admin
    admin inicio

--- src/app/calendario/calendario.component.ts
+++ src/app/calendario/calendario.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-calendario',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './calendario.component.html',
   styleUrl: './calendario.component.css'
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,6 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 
-
 export class LoginComponent {
 
   credentials = {
@@ -36,14 +35,20 @@ export class LoginComponent {
       if (this.usuarios[i].id === this.credentials.id && this.usuarios[i].password === this.credentials.password) {
         sessionStorage.setItem('usuarioId', this.usuarios[i].id);
         sessionStorage.setItem('usuarioOcupacion', this.usuarios[i].ocupacion); // Guarda la ocupación del usuario  
+
         // Redirigir a la página de perfil según el tipo de usuario
-        this.router.navigate([this.usuarios[i].ocupacion === 'alumno' ? '/perfil-alumno' : '/perfil-tutor']);
+        if (this.usuarios[i].ocupacion === 'alumno') {
+          this.router.navigate(['/perfil-alumno']);
+        } else if (this.usuarios[i].ocupacion === 'tutor') {
+          this.router.navigate(['/perfil-tutor']);
+        } else if (this.usuarios[i].ocupacion === 'admin') {
+          this.router.navigate(['/perfil-admin']);
+        }
         return;
       }
     }
-  
+
     console.log('Formulario enviado');
   }
-  
-  
+
 }
